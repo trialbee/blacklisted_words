6b644223b9167c9ec4adc9cccbe4a7b898d80e81
Jacob Burenstam <burenstam@gmail.com>
2014-12-16 20:01:08 +0100

    Don'y compute if the string is blank

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,6 @@
 class Word < ActiveRecord::Base
   def self.blacklisted_words_in(string, type = nil)
+    return [] if string.blank?
     blacklisted_words = []
     words = type.nil? ? all : where(word_type: type)
     words.each do |word|
